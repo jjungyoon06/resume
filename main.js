e0d0506640711d6becac67e9a37ba4d29b69225b
jjungyoon06 <105343136+jjungyoon06@users.noreply.github.com>
2022-09-14 15:15:35 +0900

    resume

--- main.js
+++ main.js
@@ -28,15 +28,60 @@ contactMe.addEventListener('click',()=>{
     scrollIntoView('#contact');
 });
 
-// 반복되니까 함수 설정으로 정리하고 위에 호출하기
-function scrollIntoView(selector){
-    const scrollTo = document.querySelector(selector);
-    scrollTo.scrollIntoView({behavior:'smooth'});
-}
-
 // 스크롤 내려갈수록 Home 점점 투명하게 하기
 const home = document.querySelector('#home');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll',()=>{
     home.style.opacity = 1 - window.scrollY / homeHeight;
 })
+
+//arrow 버튼으로 스크롤
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', ()=>{
+    if(window.scrollY > homeHeight /2 ){
+        arrowUp.classList.add('visible');
+    } else {
+        arrowUp.classList.remove('visible');
+    }
+});
+
+//arrow 클릭하면 home으로 올라오기
+arrowUp.addEventListener('click',()=>{
+    scrollIntoView('#home');
+});
+
+//project
+const workBtnContainer = document.querySelector('.work_categories');
+const projectContainer = document.querySelector('.work_projects');
+const projects = document.querySelectorAll('.project');
+    workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if (filter == null) {
+      return;
+    }
+  
+     // Remove selection from the previous item and select the new one
+  const active = document.querySelector('.category__btn.selected');
+  if (active != null) {
+    active.classList.remove('selected');
+  }
+  e.target.classList.add('selected');
+
+  projectContainer.classList.add('anim-out');
+  setTimeout(() => {
+    projects.forEach((project) => {
+      console.log(project.dataset.type);
+      if (filter === '*' || filter === project.dataset.type) {
+        project.classList.remove('invisible');
+      } else {
+        project.classList.add('invisible');
+      }
+    });
+    projectContainer.classList.remove('anim-out');
+  }, 300);
+});
+// 반복되니까 함수 설정으로 정리하고 위에 호출하기
+function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior:'smooth'});
+};

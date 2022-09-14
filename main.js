adc1aab099bbd72a2ad73dac6508e81d123c1879
jjungyoon06 <105343136+jjungyoon06@users.noreply.github.com>
2022-09-14 10:28:08 +0900

    resume

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
-document.addEventListener('scroll' () {
-    
-})
+'use strict'
+
+// navbar 투명하게 만들기 - 스크롤하면 원래대로 핑크색으로
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    if(window.scrollY > navbarHeight){
+        navbar.classList.add('navbar--dark');
+    }else{
+        navbar.classList.remove('navbar--dark');
+    }
+});
+
+// 메뉴, 컨택트미 누르면 스크롤되기 (html에 data-linkk="" 추가수정완료)
+const navbarMenu = document.querySelector('.navbar_menu');
+navbarMenu.addEventListener('click',(event)=>{
+     const target = event.target;
+     const link = target.dataset.link;
+     if(link == null){
+        return;
+     }
+
+     const scrollTo = document.querySelector(link);
+     scrollTo.scrollIntoView({behavior:'smooth'});
+});

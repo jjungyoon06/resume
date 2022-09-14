22c98d63ff110dceb9a7ce04ed9fb93095ea86c6
jjungyoon06 <105343136+jjungyoon06@users.noreply.github.com>
2022-09-14 18:19:44 +0900

    resume

--- main.js
+++ main.js
@@ -1,67 +1,74 @@
-'use strict'
+'use strict';
 
-// navbar 투명하게 만들기 - 스크롤하면 원래대로 핑크색으로
+// Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-    if(window.scrollY > navbarHeight){
-        navbar.classList.add('navbar--dark');
-    }else{
-        navbar.classList.remove('navbar--dark');
-    }
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark');
+  } else {
+    navbar.classList.remove('navbar--dark');
+  }
 });
 
-// navbar-menu 누르면 스크롤되기 (html에 data-link="" 추가수정완료)
+// Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar_menu');
-navbarMenu.addEventListener('click',(event)=>{
-     const target = event.target;
-     const link = target.dataset.link;
-     if(link == null){
-        return;
-     }
-   scrollIntoView(link);
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  navbarMenu.classList.remove('open');
+  scrollIntoView(link);
 });
 
-// contact 누르면 스크롤 되기
-const contactMe = document.querySelector('.home_contact');
-contactMe.addEventListener('click',()=>{
-    scrollIntoView('#contact');
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar_toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
 });
 
-// 스크롤 내려갈수록 Home 점점 투명하게 하기
-const home = document.querySelector('#home');
+// Handle click on "contact me" button on home
+const homeContactBtn = document.querySelector('.home_contact');
+homeContactBtn.addEventListener('click', () => {
+  scrollIntoView('#contact');
+});
+
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home_container');
 const homeHeight = home.getBoundingClientRect().height;
-document.addEventListener('scroll',()=>{
-    home.style.opacity = 1 - window.scrollY / homeHeight;
-})
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
 
-//arrow 버튼으로 스크롤
+// Show "arrow up" button when scrolling down
 const arrowUp = document.querySelector('.arrow-up');
-document.addEventListener('scroll', ()=>{
-    if(window.scrollY > homeHeight /2 ){
-        arrowUp.classList.add('visible');
-    } else {
-        arrowUp.classList.remove('visible');
-    }
+document.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add('visible');
+  } else {
+    arrowUp.classList.remove('visible');
+  }
 });
 
-//arrow 클릭하면 home으로 올라오기
-arrowUp.addEventListener('click',()=>{
-    scrollIntoView('#home');
+// Handle click on the "arrow up" button
+arrowUp.addEventListener('click', () => {
+  scrollIntoView('#home');
 });
 
-//project
+// Projects
 const workBtnContainer = document.querySelector('.work_categories');
 const projectContainer = document.querySelector('.work_projects');
 const projects = document.querySelectorAll('.project');
-    workBtnContainer.addEventListener('click', (e) => {
-    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
-    if (filter == null) {
-      return;
-    }
-  
-     // Remove selection from the previous item and select the new one
-  const active = document.querySelector('.category__btn.selected');
+workBtnContainer.addEventListener('click', (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+
+  // Remove selection from the previous item and select the new one
+  const active = document.querySelector('.category_btn.selected');
   if (active != null) {
     active.classList.remove('selected');
   }
@@ -80,8 +87,8 @@ const projects = document.querySelectorAll('.project');
     projectContainer.classList.remove('anim-out');
   }, 300);
 });
-// 반복되니까 함수 설정으로 정리하고 위에 호출하기
-function scrollIntoView(selector){
-    const scrollTo = document.querySelector(selector);
-    scrollTo.scrollIntoView({behavior:'smooth'});
-};
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
